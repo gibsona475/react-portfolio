67e5413d3af03033cfdbaf63653e98d676c835a0
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-09-27 15:30:38 -0700

    Updated file structure

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import NavBar from './components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React portfolio</h1>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function NavBar() {
+    return (<h2> Here comes my Navbar</h2>
+    )
+}
+
+export default NavBar;
